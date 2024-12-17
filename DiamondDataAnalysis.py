9ecf16a32d2c8ed844d0c8e68e03a0606f318bdc
singhsitanshu <singhsitanshu2006@gmail.com>
2024-12-17 00:21:55 -0600

    imported dataset

--- DiamondDataAnalysis.py
+++ DiamondDataAnalysis.py
@@ -1,5 +1,10 @@
 import numpy as np
 import pandas as pd
-
 import os
 
+#C:\Users\singh\.cache\kagglehub\datasets\shivam2503\diamonds\versions\1\diamonds.csv
+
+train = pd.read_csv(r'C:\Users\singh\.cache\kagglehub\datasets\shivam2503\diamonds\versions\1\diamonds.csv')
+df=train.copy()
+df.head()
+df.tail()
