f41a209a682058f67a5ae3b78869336ef0abc51d
singhsitanshu <singhsitanshu2006@gmail.com>
2024-12-17 00:26:03 -0600

    import packages

--- DiamondDataAnalysis.py
+++ DiamondDataAnalysis.py
@@ -1,10 +1,12 @@
 import numpy as np
 import pandas as pd
-import os
+import matplotlib.pyplot as plot
+import seaborn
 
 #C:\Users\singh\.cache\kagglehub\datasets\shivam2503\diamonds\versions\1\diamonds.csv
 
-train = pd.read_csv(r'C:\Users\singh\.cache\kagglehub\datasets\shivam2503\diamonds\versions\1\diamonds.csv')
-df=train.copy()
+df = pd.read_csv(r'C:\Users\singh\.cache\kagglehub\datasets\shivam2503\diamonds\versions\1\diamonds.csv')
+
 df.head()
-df.tail()
+
+df.describe()
