a502d786ee2596a556947843cfee1dc1dddbec30
singhsitanshu <singhsitanshu2006@gmail.com>
2024-12-18 21:14:25 -0600

    handling outliers and missing values

--- DiamondDataAnalysis.py
+++ DiamondDataAnalysis.py
@@ -19,3 +19,14 @@ df.shape
 df['cut'].unique()
 df['color'].unique()
 df['clarity'].unique()
+
+s_25 = df['price'].quantile(0.25)
+s_75 = df['price'].quantile(0.75)
+IQR = s_75-s_25
+
+s_lower = max(s_25 - IQR*1.5, df['price'].min())
+s_upper = min(s_75 + IQR*1.5, df['price'].max())
+df = df[(df['price']>=s_lower) & (df['price']<=s_upper)]
+df.shape
+
+df.isna().sum()*100/df.shape[0]
